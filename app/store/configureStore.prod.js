a01b3815982e02a860054f08357e5b46085e249b
wlianfu <wlianfu@163.com>
2019-06-13 21:36:02 +0800

    optimize 2

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -4,15 +4,14 @@ import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from '../reducers';
-import type { counterStateType } from '../reducers/types';
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
-function configureStore(initialState?: counterStateType) {
-  return createStore<*, counterStateType, *>(
+function configureStore(initialState?: any) {
+  return createStore<*, *, *>(
     rootReducer,
     initialState,
     enhancer
